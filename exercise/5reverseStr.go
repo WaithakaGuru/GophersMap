1939a14eff04acf42841621e4590d38b47c074a2
WaithakaGuru <waithakaoffices@gmail.com>
2025-12-09 00:18:39 +0300

    Add exercise 5: reverse a string

---
+++ exercise/5reverseStr.go
@@ -0,0 +1,18 @@
+/* Exercise Five
+5. Write a function that reverses a string.
+*/
+
+package exercise
+
+func ReverseString(str string) string {
+	// two pointer approach
+	runeStr := []rune(str)
+	i, j := 0, len(runeStr)-1
+
+	for i <= j {
+		temp := runeStr[i]
+		runeStr[i] = runeStr[j]
+		runeStr[j] = temp
+	}
+	return string(runeStr)
+}
