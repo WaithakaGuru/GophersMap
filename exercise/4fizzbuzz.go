5a5615589a3c823dfaaf3a347d88fe3b9893670e
WaithakaGuru <waithakaoffices@gmail.com>
2025-12-09 00:05:30 +0300

    Add the fizzbuzz exercise and change the file naming format

---
+++ exercise/4fizzbuzz.go
@@ -0,0 +1,20 @@
+/*
+	Exercise Four
+
+4. Print numbers from 1 to 100. For multiples of 3 print "Fizz", for multiples of 5 print "Buzz", and for both print "FizzBuzz".
+*/
+package exercise
+
+import "fmt"
+
+func FizBuzz() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		}
+	}
+}
