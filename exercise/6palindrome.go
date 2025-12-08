701f90f1f1ed45278f66043622231cdfb0215516
WaithakaGuru <waithakaoffices@gmail.com>
2025-12-09 00:22:56 +0300

    Add exercise 6: Palindrome checker

---
+++ exercise/6palindrome.go
@@ -0,0 +1,17 @@
+/* Exercise Six
+6. Check if a given string is a palindrome.
+*/
+
+package exercise
+
+func isPalindrome(str string) bool {
+	// use two pointer method
+	i, j := 0, len(str)-1
+
+	for i <= j {
+		if !(str[i] == str[j]) {
+			return false
+		}
+	}
+	return true
+}
